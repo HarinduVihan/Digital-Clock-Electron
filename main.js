cba8e554b2cec59eeaa1c863d157c82edbc3f8a2
haribduvihan <harinduvihan@gmail.com>
2025-05-20 19:23:20 +0530

    added to the tray

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-const { app, BrowserWindow } = require("electron");
+//const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu, Tray } = require("electron");
+
+let win;
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 150,
     frame: false,
@@ -15,78 +18,32 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+  tray = new Tray("./Gold Luxury Initial Logo (1).png");
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show Digital Clock",
+      type: "radio",
+      checked: true, // Initially selected
+      click: () => {
+        win.show();
+      },
+    },
+    {
+      label: "Hide Digital Clock",
+      type: "radio",
+      click: () => {
+        win.hide();
+      },
+    },
+    {
+      label: "Close Digital Clock",
+      click: () => {
+        app.quit(); // actually quit the app.
+      },
+    },
+  ]);
+  tray.setToolTip("Digital Clock");
+  tray.setContextMenu(contextMenu);
 });
 
 app.setLoginItemSettings({ openAtLogin: true });
-
-//---------------------------
-// const path = require('path')
-// const Electrolytic = require('electrolytic')
-// const { app, BrowserWindow, Menu, nativeImage, Tray } = require('electron')
-
-// let tray = null
-// function createTray () {
-//   const icon = path.join(__dirname, '/app.png') // required.
-//   const trayicon = nativeImage.createFromPath(icon)
-//   tray = new Tray(trayicon.resize({ width: 16 }))
-//   const contextMenu = Menu.buildFromTemplate([
-//     {
-//       label: 'Show App',
-//       click: () => {
-//         createWindow()
-//       }
-//     },
-//     {
-//       label: 'Quit',
-//       click: () => {
-//         app.quit() // actually quit the app.
-//       }
-//     },
-//   ])
-
-//   tray.setContextMenu(contextMenu)
-// }
-
-// const ELECTROLYTIC_APP_KEY = '<Your-app-Key>'
-
-// const electrolytic = Electrolytic({
-//   appKey: ELECTROLYTIC_APP_KEY,
-// })
-
-// electrolytic.on('token', token => { // will be called everytime on app start
-//   console.log('user token', token);
-//   mainWindow && mainWindow.webContents.send('token', token)
-// })
-
-// electrolytic.on('push', (payload) => { // when you use the API to send a push
-//   console.log('got push notification in main', payload)
-//   mainWindow && mainWindow.webContents.send('push', payload)
-// })
-
-// let mainWindow
-
-// function createWindow () {
-//   if (!tray) { // if tray hasn't been created already.
-//     createTray()
-//   }
-
-//   mainWindow = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     webPreferences: {
-//       nodeIntegration: true // not cool. I'm sorry.
-//     }
-//   })
-
-//   mainWindow.loadFile('index.html')
-//   mainWindow.on('closed', function () {
-//     mainWindow = null
-//   })
-// }
-
-// app.on('ready', createWindow)
-
-// app.on('window-all-closed', () => {
-//   app.dock.hide()
-//   // any other logic
-// })
